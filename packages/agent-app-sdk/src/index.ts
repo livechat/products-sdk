492addf271eda5c98762f7543f3eb5e9f89aa823
Sambor Górnicz <s.gornicz@livechatinc.com>
2024-10-31 11:51:40 +0100

    Export more payment types

--- packages/agent-app-sdk/src/index.ts
+++ packages/agent-app-sdk/src/index.ts
@@ -1,4 +1,4 @@
-export { Charge, IDirectCharge, IRecurrentCharge, Metadata } from '@livechat/widget-core-sdk';
+export { Charge, IDirectCharge, IRecurrentCharge, Metadata, TransactionError, TransactionEvent, UpdateBillingCycleEvent } from '@livechat/widget-core-sdk';
 export * from './widgets/details';
 export * from './widgets/fullscreen';
 export * from './widgets/messagebox';
