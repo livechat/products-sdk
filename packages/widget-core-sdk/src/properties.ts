0f6c935131981d5b397d57470c94f86f46e622a3
pastepi <p.stepien@livechat.com>
2025-09-29 11:36:29 +0200

    refactor: better types

--- packages/widget-core-sdk/src/properties.ts
+++ packages/widget-core-sdk/src/properties.ts
@@ -4,13 +4,20 @@ enum PropertiesEvents {
   UpdateProperties = 'update_properties'
 }
 
+export interface IUpdateProperties {
+  onboarded?: boolean;
+  plan?: string;
+  error_code?: string;
+  [key: string]: string | number | boolean | null | undefined;
+}
+
 export interface IPropertiesApi {
-  updateProperties(properties?: object): Promise<void>;
+  updateProperties(properties?: IUpdateProperties): Promise<void>;
 }
 
 export const withProperties: WidgetMixin<IPropertiesApi, void> = widget => ({
   ...widget,
-  updateProperties(properties?: object): Promise<void> {
+  updateProperties(properties?: IUpdateProperties): Promise<void> {
     return widget.sendMessage(PropertiesEvents.UpdateProperties, properties);
   }
 });
