9ee3fbec71082c4ba29fe16f0419359c22a69823
Paweł Stępień <57643748+pastepi@users.noreply.github.com>
2025-09-29 12:27:39 +0200

    ME-703 Improve `updateProperties` method types (#39)
    * refactor: better types
    
    * changeset
    
    * version packages & install
    
    * fix: type
    
    * use Record syntax

--- packages/widget-core-sdk/src/properties.ts
+++ packages/widget-core-sdk/src/properties.ts
@@ -4,13 +4,19 @@ enum PropertiesEvents {
   UpdateProperties = 'update_properties'
 }
 
+export type IUpdateProperties = {
+  onboarded?: boolean;
+  plan?: string;
+  error_code?: string | null;
+} & Record<string, string | number | boolean | null | undefined>;
+
 export interface IPropertiesApi {
-  updateProperties(properties?: object): Promise<void>;
+  updateProperties(properties?: IUpdateProperties): Promise<void>;
 }
 
 export const withProperties: WidgetMixin<IPropertiesApi, void> = widget => ({
   ...widget,
-  updateProperties(properties?: object): Promise<void> {
+  updateProperties(properties?: IUpdateProperties): Promise<void> {
     return widget.sendMessage(PropertiesEvents.UpdateProperties, properties);
   }
 });
